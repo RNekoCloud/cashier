a43f53869578e721c44247ada82924011ba5322e
RageNeko26 <rneko2006@gmail.com>
2024-02-06 16:13:21 +0700

    feat: Add API Response message and hash password

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,30 +1,53 @@
 import sequelize from "@/config/database";
 import User from "@/model/user";
+// yarn add bcrypt
+import bcrypt from "bcrypt";
+import UserService from "@/service/UserService";
 
 // POST http://localhost:3000/api/users
 
 export default async function POST(req, res) {
     try {
-        // Konek ke database
-        await sequelize.authenticate();
+        // Ambil data asli password
+        const pass = req.body.password;
+        
+        // Generate salt untuk menghasilkan hash password
+        const salt = bcrypt.genSaltSync(12);
 
-        // Migrate Up
-        await sequelize.sync();
+        // Hash password
+        const hash = bcrypt.hashSync(pass, salt);
 
-        const username = req.body.username;
-        const password = req.body.password;
-        const role = req.body.role;
+        // Initialisasi class instance
+        const userService = new UserService(sequelize, User);
+        // const foo = {}
+        // foo = {
+        //     sequelize: Sequelize,
+        //      user_model: User
+        //   }
 
-        // Query insert
-        const result = await User.create({
-            username: username,
-            password: password,
-            role: role,
+        const register = await userService.store({
+            username: req.body.username,
+            // Bukan req.body.password
+            password: hash,
+            role: req.body.role,
         })
 
-        res.json(result)
+        // HTTP Status Code
+        // 200 - OK (Biasa digunakan saat mengambil data)
+        // 201 - Created(Biasa digunakan saat menambah data)
+        // 500 - Internal Server Error (Gagal menambah database, koneksi error database dlll)
+
+
+        return res.status(201).json({
+            message: "Berhasil mendaftarkan user baru",
+            result: register,
+        })
 
     } catch(error) {
         console.log(error)
+        return res.status(500).json({
+            message: "Gagal daftar user karena internal error",
+        })
+
     }
 }
