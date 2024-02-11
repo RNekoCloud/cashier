a4e962f820f14cba83bc3f50b6981e692736faf6
RageNeko26 <rneko2006@gmail.com>
2024-02-11 22:55:39 +0700

    feat: Add model schema for Product

---
+++ model/product.js
@@ -0,0 +1,23 @@
+import sequelize from "@/config/database";
+import { DataTypes } from "sequelize";
+
+const Product = sequelize.define("product", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+  },
+  description: {
+    type: DataTypes.STRING,
+  }
+});
+
+export default Product;
