a87c5f4d916c054ca17b7854948c8b6bde09477c
RageNeko26 <rneko2006@gmail.com>
2024-02-12 11:42:09 +0700

    chore: Prettying code

--- app/products/page.js
+++ app/products/page.js
@@ -1,16 +1,15 @@
 import Sidebar from "@/components/Sidebar";
 import { Container } from "react-bootstrap";
 
-
 export default function Products() {
   return (
     <>
       <Sidebar />
-      <main style={{marginTop: '58px'}}>
+      <main style={{ marginTop: "58px" }}>
         <Container className="pt-4">
           <h2>Hello world</h2>
         </Container>
       </main>
     </>
-  )
+  );
 }
